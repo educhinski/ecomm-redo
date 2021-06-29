c7ce90f57943c9247e120b6c7b20c1e9ba822712
educhinski <educhinski650@gmail.com>
2021-06-29 10:49:03 +0300

    Added account page components and fixed assets error

--- src/app/modules/user/account/account.module.ts
+++ src/app/modules/user/account/account.module.ts
@@ -4,14 +4,44 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AccountHomeComponent } from './account-home/account-home.component';
 import { OrdersComponent } from './orders/orders.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { CreditComponent } from './credit/credit.component';
+import { SavedItemsComponent } from './saved-items/saved-items.component';
+import { RecentComponent } from './recent/recent.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
+import { AddressComponent } from './address/address.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { PasswordComponent } from './password/password.component';
 
 const routes: Routes = [
   { path: 'orders', component: OrdersComponent },
+  { path: 'inbox', component: InboxComponent },
+  { path: 'reviews', component: ReviewsComponent },
+  { path: 'credit', component: CreditComponent },
+  { path: 'saved-items', component: SavedItemsComponent },
+  { path: 'recent', component: RecentComponent },
+  { path: 'details', component: AccountDetailsComponent },
+  { path: 'address', component: AddressComponent },
+  { path: 'password', component: PasswordComponent },
+  { path: 'newsletter', component: NewsletterComponent },
   { path: '', component: AccountHomeComponent },
 ];
 
 @NgModule({
-  declarations: [AccountHomeComponent, OrdersComponent],
+  declarations: [
+    AccountHomeComponent,
+    OrdersComponent,
+    InboxComponent,
+    ReviewsComponent,
+    CreditComponent,
+    SavedItemsComponent,
+    RecentComponent,
+    AccountDetailsComponent,
+    AddressComponent,
+    NewsletterComponent,
+    PasswordComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class AccountModule {}
--- src/app/modules/user/account/orders/orders.component.ts
+++ src/app/modules/user/account/orders/orders.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
+  imagePath = '../../../../../assets/images/';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
