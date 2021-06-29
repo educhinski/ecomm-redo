1fd5ea75fbafb6568c721b55016c35ca15f1bcc8
educhinski <educhinski650@gmail.com>
2021-06-29 08:09:22 +0300

    Setup Header & Footer Shared Components

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -8,6 +8,7 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onClick(val: string): void {
+    this.router.navigate([`/user/${val}`]);
   }
 
+  ngOnInit(): void {}
 }
