5901b7e188898d1e8d419da28c97ceefe45032e7
educhinski <educhinski650@gmail.com>
2021-06-30 02:11:51 +0300

    Setup grid shared component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductGridComponent } from './product-grid/product-grid.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, ProductGridComponent],
   imports: [CommonModule, SharedRoutingModule],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, ProductGridComponent],
 })
 export class SharedModule {}
