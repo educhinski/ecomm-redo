8d1d24cc5d73773a36a1b300e757fad1e17407ff
educhinski <educhinski650@gmail.com>
2021-07-06 16:12:12 +0300

    Admin overview

--- src/app/modules/admin/overview/overview.component.ts
+++ src/app/modules/admin/overview/overview.component.ts
@@ -8,46 +8,5 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./overview.component.css'],
 })
 export class OverviewComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
-        ];
-      }
-
-      return [
-        { title: 'Card A', cols: 1, rows: 1 },
-        { title: 'Card B', cols: 2, rows: 1 },
-        { title: 'Card C', cols: 1, rows: 1 },
-        { title: 'Card D', cols: 1, rows: 1 },
-      ];
-    })
-  );
-
-  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return {
-          columns: 1,
-          miniCard: { cols: 1, rows: 1 },
-          chart: { cols: 1, rows: 2 },
-          table: { cols: 1, rows: 4 },
-        };
-      }
-
-      return {
-        columns: 4,
-        miniCard: { cols: 4, rows: 1 },
-        chart: { cols: 4, rows: 2 },
-        table: { cols: 4, rows: 4 },
-      };
-    })
-  );
-
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
