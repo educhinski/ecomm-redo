131dca6e8005279642d24347098e3734b128af1b
educhinski <educhinski650@gmail.com>
2021-07-12 10:59:57 +0300

    Highlight, Brands link, Product grid carousel

---
+++ src/app/shared/helpers/categories.ts
@@ -0,0 +1,31 @@
+const categories: string[] = [
+  'supermarket',
+  'health-beauty',
+  'home-office',
+  'phones-tablets',
+  'computing',
+  'electronics',
+  'fashion',
+  'gaming',
+  'baby-products',
+  'sporting-goods',
+  'garden-outdoors',
+  'other-categories',
+];
+
+const categoriesHeadings: string[] = [
+  'supermarket',
+  'health & beauty',
+  'home & office',
+  'phones & tablets',
+  'computing',
+  'electronics',
+  'fashion',
+  'gaming',
+  'baby products',
+  'sporting goods',
+  'garden & outdoors',
+  'other categories',
+];
+
+export { categories, categoriesHeadings };
