371f72d79233dd0092893b3f107f3a5141c0d816
educhinski <educhinski650@gmail.com>
2021-07-12 14:12:54 +0300

    Admin invoice table

---
+++ src/app/modules/admin/admin-sellers/invoices-table/invoices-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-invoices-table',
+  templateUrl: './invoices-table.component.html',
+  styleUrls: ['./invoices-table.component.css'],
+})
+export class InvoicesTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
