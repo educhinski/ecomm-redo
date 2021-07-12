558796dc4b84bc76aa784cbbe27778708d9ba39a
educhinski <educhinski650@gmail.com>
2021-07-12 11:59:12 +0300

    Modified help

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,21 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductGridComponent } from './components/product-grid/product-grid.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, ProductGridComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ProductGridComponent,
+    HelpComponent,
+  ],
   imports: [CommonModule, SharedRoutingModule],
-  exports: [HeaderComponent, FooterComponent, ProductGridComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    ProductGridComponent,
+    HelpComponent,
+  ],
 })
 export class SharedModule {}
