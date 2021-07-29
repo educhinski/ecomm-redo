f80ba5882304cbf2d3e5c628c1336a507c1c2a8b
Edwin Mambo <educhinski650@gmail.com>
2021-07-29 12:03:59 +0300

    Setup httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,11 +11,23 @@ import { BrandsComponent } from './components/brands/brands.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { HighlightsComponent } from './components/highlights/highlights.component';
 import { CartComponent } from './components/cart/cart.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, DefaultComponent, BrandsComponent, CarouselComponent, HighlightsComponent, CartComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    DefaultComponent,
+    BrandsComponent,
+    CarouselComponent,
+    HighlightsComponent,
+    CartComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
