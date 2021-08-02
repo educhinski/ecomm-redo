9656eca348c27b55bcfcbf5622100f33aaeb484a
Edwin Mambo <educhinski650@gmail.com>
2021-08-02 12:44:28 +0300

    Products API working

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,15 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private REST_API_SERVER = 'http://localhost:3000';
+  private REST_API_SERVER = 'https://peaceful-beyond-74495.herokuapp.com/api';
 
   constructor(private httpClient: HttpClient) {}
 
-  public getProducts() {
-    return this.httpClient.get(this.REST_API_SERVER + '/products');
+  public getProducts(): Observable<any> {
+    return this.httpClient
+      .get(this.REST_API_SERVER + '/products')
+      .pipe(catchError(this.handleError));
+  }
+
+  // handle error
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred: ', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later');
   }
 }
--- src/app/shared/components/product-grid/product-grid.component.ts
+++ src/app/shared/components/product-grid/product-grid.component.ts
@@ -10,14 +10,37 @@ import { categories, categoriesHeadings } from '../../helpers/categories';
 export class ProductGridComponent implements OnInit {
   categories = categories;
   headings = categoriesHeadings;
+  productsData: any[] = [];
   products: any[] = [];
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.getProducts().subscribe((data: any) => {
-      console.log(data);
-      this.products = data;
+      this.productsData = data.data;
+      this.products = this.getProductList(this.productsData);
     });
+  }
+
+  getProductList(data: any) {
+    let products: any[] = [];
+    for (const datum in data) {
+      if (Object.prototype.hasOwnProperty.call(data, datum)) {
+        const product = data[datum].data;
+
+        // add each item to products array
+        products.push({
+          id: product.product_id,
+          name: product.attributes.product_name,
+          price: product.attributes.product_price,
+          currency: product.attributes.currency,
+          description: product.attributes.product_details,
+          discountPercent: product.attributes.discount_percent,
+          rating: product.attributes.productReviews.meta.review_count,
+          imagePath: product.attributes.cover_image.data.attributes.path,
+        });
+      }
+    }
+    return products;
   }
 }
